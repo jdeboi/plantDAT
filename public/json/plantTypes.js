52763d0dc0d9b207d2dad569f5a9f26d8a00e89c
Jenna deBoisblanc <jdeboi@gmail.com>
2019-05-17 13:55:17 -0500

    names caps oops

--- public/json/plantTypes.js
+++ public/json/plantTypes.js
@@ -2,14 +2,14 @@ const plantTypes = {
   "sweet berry": {
     "latin_name": "sargasus whateverus",
     "bio": "here are some amazing things about this plant. here's how it grows. what its flowers look like. how to care for it? special powers. other things of note. I dunno how many words are we trying to go for here stream of conciousness.",
-    "img_path": "plantImg.png"
+    "img_path": "plantimg.png"
 
   },
 
   "firey bush": {
     "latin_name": "lolygagus hoopydoodadus",
     "bio": "here are some amazing things about this plant. here's how it grows. what its flowers look like. how to care for it? special powers. other things of note. I dunno how many words are we trying to go for here stream of conciousness.",
-    "img_path": "plantImg2.png"
+    "img_path": "plantimg2.png"
   }
 }
 
