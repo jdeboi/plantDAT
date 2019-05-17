4affa3272155ccd5f64fa16d9e724bea3f610cde
Jenna deBoisblanc <jdeboi@gmail.com>
2019-05-17 12:53:57 -0500

    i think just needed to make it listen ...

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var createError = require('http-errors');
+
+const PORT = process.env.PORT || 5000
+
+
+
 var express = require('express');
+var app = express();
+
+var createError = require('http-errors');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var moment = require('moment');
 var logger = require('morgan');
 // var io = require('socket.io')(http);
+
+
 
 var indexRouter = require('./routes/index');
 var plantRouter = require('./routes/plant');
@@ -14,7 +24,6 @@ var app = express();
 
 // database
 var mongoose = require('mongoose');
-console.log(process.env['MONGO_URI']);
 var mongoDB = process.env['MONGO_URI'];
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
@@ -52,5 +61,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 module.exports = app;
