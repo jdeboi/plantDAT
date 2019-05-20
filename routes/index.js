7068825c48ed2671bb0c16a09dfb0d4b540a12ab
Jenna deBoisblanc <jdeboi@gmail.com>
2019-05-20 00:04:41 -0500

    buku changes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 router.get('/resources', function(req, res, next) {
-  res.render('resources', {});
+  res.render('resources', { title: 'Resources' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', {});
+  res.render('about', { title: 'About' });
+});
+
+router.get('/spawn', function(req, res, next) {
+  res.render('spawn', { title: 'Spawn' });
 });
 
 module.exports = router;
