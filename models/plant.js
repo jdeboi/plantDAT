c2d24ca10fa941a24863a71acee477421591ed12
Jenna deBoisblanc <jdeboi@gmail.com>
2019-05-20 08:18:56 -0500

    finishing up web; working on spawning post

--- models/plant.js
+++ models/plant.js
@@ -3,17 +3,56 @@
 var mongoose = require('mongoose');
 
 var plantSchema = new mongoose.Schema({
-  code: String,
-  name: String,
+
+  x: Number,
+  y: Number,
+  color: String,
+  // name: String,
   // plantType: ObjectId,
   plantType: String,
   created_at: Date,
   height: Number,
+  spawned: Boolean,
+
+  code: String,
   health: Number,
+  growthRate: Number,
   waterStored: Number,
-  spawned: Boolean
+  sunlight: Number,
+  water: Number,
+  drainage: Number
+
 }, { collection : 'plants' });
 
 var Plant = mongoose.model('Plant', plantSchema);
+
+Plant.methods.create = function create(type, callback) {
+  let num = await Plant.count();
+  var plant = new Plant({
+    x: 0.5,
+    y: 0.5,
+    color: getRandomColor(),
+    plantType: type,
+    created_at: new Date(),
+    height: 0,
+    spawned: true,
+
+    code: num,
+    health: 1.0,
+    growthRate: 1.0,
+    waterStored: 0,
+    sunlight: 0.5,
+    water: 0.5,
+    drainage: 0.5,
+
+  });
+  plant.save(callback);
+}
+
+function getRandomColor() {
+  let colors = ["#1fe226", "#ff1990", "#f49e42"];
+  let index = Math.floor(Math.random()*colors.length);
+  return colors[index];
+}
 
 module.exports = Plant;
--- routes/api.js
+++ routes/api.js
@@ -4,28 +4,19 @@ var moment = require('moment');
 var Plant = require('../models/plant')
 var plantTypeJson = require('../public/json/plantTypes');
 
-router.get('/plant/:id', function(req, res, next) {
-  var id = req.params.id;
-  console.log("id: " + id);
-  Plant
-  .findOne({code:id})
-  .exec(function (err, doc) {
-    if (err) {
-      res.send(err);
-    }
-    if (doc != null) {
-      let plant = {
-        plant: doc,
-        type: plantTypeJson[doc.name]
-      };
-      res.json(plant);
-    }
-    else {
-      res.json({});
-    }
-  });
-  // res.render('plant', { plant: id });
+router.get('/spawn', function(req, res, next) {
+  res.render('spawn', { title: 'Spawn' });
 });
+
+router.post('/spawn/:plantType', function(req, res, next) {
+  var plantType = req.params.plantType;
+  console.log("spawn new plant type: " + plantType);
+  Plant.create(plantType, (err) => {
+    if (err) throw err;
+    res.render('index', { title: 'Home' });
+  });
+
+
 
 router.get('/allspawned', function(req, res, next) {
   Plant
@@ -42,7 +33,7 @@ router.get('/allspawned', function(req, res, next) {
         let now = (Date.now());
         let born = new Date(moment(plant.created_at).format("YYYY-MM-DD HH:mm"))
         let diffHours = (now - born)/1000/60/60;
-        let roundedHours = diffHours.toFixed(1);
+        let roundedHours = diffHours.toFixed(2);
         plants.push({
           plant: plant,
           age: roundedHours,
@@ -56,31 +47,57 @@ router.get('/allspawned', function(req, res, next) {
     }
 
   });
-  // res.render('plant', { plant: id });
 });
 
-router.post('/spawnplant/:id', function(req, res, next) {
-  var id = req.params.id;
-  Plant
-  .findOne({code:id}, function (err, plant) {
-    if (err) {
-      res.send(err);
-    }
-    if (plant != null) {
-      plant.spawned = true;
-      plant.created_at = new Date();
-      plant.save();
-      return res.status(201).send({
-        success: 'true',
-        message: 'plant spawned successfully', plant
-      })
-    }
-    return res.status(400).send({
-      success: 'false',
-      message: 'no plant found'
-    });
+// router.post('/spawnplant/:id', function(req, res, next) {
+//   var id = req.params.id;
+//   Plant
+//   .findOne({code:id}, function (err, plant) {
+//     if (err) {
+//       res.send(err);
+//     }
+//     if (plant != null) {
+//       plant.spawned = true;
+//       plant.created_at = new Date();
+//       plant.save();
+//       return res.status(201).send({
+//         success: 'true',
+//         message: 'plant spawned successfully', plant
+//       })
+//     }
+//     return res.status(400).send({
+//       success: 'false',
+//       message: 'no plant found'
+//     });
+//
+//   });
+// });
 
-  });
-});
+// router.get('/:id', function(req, res, next) {
+//   var id = req.params.id;
+//   console.log("id: " + id);
+//   Plant
+//   .findOne({code:id})
+//   .exec(function (err, doc) {
+//     if (err) {
+//       console.log("not working: " + error);
+//       return console.error(err);
+//     }
+//     else {
+//       if (doc) {
+//
+//         res.render('plant', {
+//           plant: doc,
+//           plantType: plantTypeJson[doc.name],
+//           moment: moment
+//         });
+//       }
+//       else {
+//         res.render('notfound', {error: "no such plant"});
+//       }
+//     }
+//   });
+//   // res.render('plant', { plant: id });
+// });
 
 module.exports = router;
