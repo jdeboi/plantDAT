66f0f2a8064b8961f255f11aed806c860adea8aa
Jenna deBoisblanc <jdeboi@gmail.com>
2019-06-07 15:12:36 -0500

    cycle home

--- public/javascripts/digging.js
+++ public/javascripts/digging.js
@@ -146,7 +146,7 @@ function spawn() {
     'x': sendx,
     'y': sendy
   };
-  httpPost(url, postData,()=> { console.log("spawned"); window.location.href = "/thanks"   }, ()=> { console.log("spawn failed")});
+  httpPost(url, postData,()=> { console.log("spawned"); window.location.href = "/"   }, ()=> { console.log("spawn failed")});
 
 }
 
