f34060fb9b72c0e8233a6e5ab8222074c2b9e542
Jenna deBoisblanc <jdeboi@gmail.com>
2019-09-26 22:29:13 -0500

    dot env?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const PORT = process.env.PORT || 5000
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 
 var express = require('express');
